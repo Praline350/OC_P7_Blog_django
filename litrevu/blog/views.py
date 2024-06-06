c72c24ec902e26db942359e8466339389831acd5
edwin thierry <edwin.thierry350@gmail.com>
2024-06-06 12:36:08 +0200

    review edit

--- litrevu/blog/views.py
+++ litrevu/blog/views.py
@@ -16,13 +16,17 @@ from blog.forms import TicketForm, TicketEditForm, ImageForm, ReviewForm
 def home(request):
     tickets = Ticket.objects.all()
     reviews = Review.objects.all()
+    star_range = range(1, 6)
     context = {
-        'tickets': tickets,
-        'reviews': reviews
+        "tickets": tickets,
+        "reviews": reviews,
+        "star_range": star_range,
     }
     for ticket in tickets:
-        ticket.is_creator = (ticket.user == request.user)
-    return render(request, 'blog/home.html', context=context)
+        ticket.is_creator = ticket.user == request.user
+    for review in reviews:
+        review.is_creator = review.user == request.user
+    return render(request, "blog/home.html", context=context)
 
 
 class TicketUploadView(LoginRequiredMixin, View):
@@ -34,11 +38,11 @@ class TicketUploadView(LoginRequiredMixin, View):
         ticket_form = self.ticket_form_class()
         image_form = self.image_form_class()
         context = {
-            'image_form': image_form,
-            'ticket_form': ticket_form,
+            "image_form": image_form,
+            "ticket_form": ticket_form,
         }
         return render(request, self.template_name, context=context)
-    
+
     def post(self, request):
         ticket_form = self.ticket_form_class(request.POST, request.FILES)
         image_form = self.image_form_class(request.POST, request.FILES)
@@ -48,46 +52,46 @@ class TicketUploadView(LoginRequiredMixin, View):
             ticket.user = request.user
             ticket.save()
             image_form.save()
-            context = {
-                'image_form': image_form,
-                'ticket_form': ticket_form,
-            }
-            return redirect('home')
+            return redirect("home")
         else:
             messages.error(request, "le formulaire n'est pas valides")
+            context = {
+                "image_form": image_form,
+                "ticket_form": ticket_form,
+            }
         return render(request, self.template_name, context=context)
 
 
 class TicketEditView(LoginRequiredMixin, UpdateView):
     model = Ticket
     form_class = TicketEditForm
-    template_name = 'blog/ticket_edit.html'
-    success_url = reverse_lazy('home')
+    template_name = "blog/ticket_edit.html"
+    success_url = reverse_lazy("home")
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user=self.request.user)
-    
+
 
 class TicketDeleteView(LoginRequiredMixin, DeleteView):
     model = Ticket
-    template_name = 'blog/ticket_delete.html'
-    success_url = reverse_lazy('home')
+    template_name = "blog/ticket_delete.html"
+    success_url = reverse_lazy("home")
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user=self.request.user)
-    
+
 
 class ReviewUploadView(LoginRequiredMixin, View):
-    template_name = 'blog/review_upload.html'
+    template_name = "blog/review_upload.html"
     form_class = ReviewForm
 
     def get(self, request, pk):
         ticket = Ticket.objects.get(pk=pk)
         form = self.form_class(ticket=ticket)
-        return render(request, self.template_name, context={'form': form})
-    
+        return render(request, self.template_name, context={"form": form})
+
     def post(self, request, pk):
         ticket = Ticket.objects.get(pk=pk)
         form = self.form_class(request.POST, ticket=ticket)
@@ -96,5 +100,26 @@ class ReviewUploadView(LoginRequiredMixin, View):
             review.user = request.user
             review.ticket = ticket
             review.save()
-            return redirect('home')
-        return render(request, self.template_name, context={'form': form})
+            return redirect("home")
+        return render(request, self.template_name, context={"form": form})
+
+
+class ReviewEditView(LoginRequiredMixin, UpdateView):
+    model = Review
+    form_class = TicketEditForm
+    template_name = "blog/review_edit.html"
+    success_url = reverse_lazy("home")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+
+class ReviewDeleteView(LoginRequiredMixin, DeleteView):
+    model = Review
+    template_name = "blog/review_delete.html"
+    success_url = reverse_lazy("home")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
