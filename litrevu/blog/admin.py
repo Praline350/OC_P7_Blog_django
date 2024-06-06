681bfdd191774afeae9169de76a35949445c2ad5
edwin thierry <edwin.thierry350@gmail.com>
2024-06-06 09:54:20 +0200

    fichiers dependance no commit

--- litrevu/blog/admin.py
+++ litrevu/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Ticket, Review, UserFollows
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'user')
+
+
+admin.site.register(UserFollows)
